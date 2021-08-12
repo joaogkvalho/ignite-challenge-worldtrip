4332b6311ba77856b00979e9823bcce115c40cd4
joao gabriel <joaogkvalho@gmail.com>
2021-08-12 15:18:33 -0300

    features added

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,4 +1,4 @@
-import { Image } from '@chakra-ui/react'
+import { Image, Text } from '@chakra-ui/react'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Navigation, Pagination } from 'swiper/core'
@@ -10,15 +10,70 @@ SwiperCore.use([Navigation, Pagination])
 
 export function Slider(){
     return(
-        <Swiper navigation={true} pagination={true} className={styles.slider}>
+        <Swiper
+          navigation={true}
+          pagination={true}
+          className={styles.slider}
+        >
             <SwiperSlide>
-                <Image src="/images/slide01.jpg" objectFit="cover" />
+                <Image
+                  src="/images/slide01.jpg"
+                  w="100%"
+                  h="100%"
+                  objectFit="cover" 
+                  position="absolute"
+                />
+                <Text
+                  fontSize="3xl"
+                  fontWeight="700"
+                  color="white"
+                  position="relative"
+                  align="center"
+                  mt="44"
+                >
+                  Europa<br/>
+                  O continente mais antigo
+                </Text>
             </SwiperSlide>
             <SwiperSlide>
-                <Image src="/images/slide02.jpg" objectFit="cover" />
+                <Image
+                  src="/images/slide02.jpg"
+                  w="100%"
+                  h="100%"
+                  objectFit="cover" 
+                  position="absolute"
+                />
+                 <Text
+                  fontSize="3xl"
+                  fontWeight="700"
+                  color="white"
+                  position="relative"
+                  align="center"
+                  mt="44"
+                >
+                  Ásia<br/>
+                  O continente da tecnologia
+                </Text>
             </SwiperSlide>
             <SwiperSlide>
-                <Image src="/images/slide03.jpg" objectFit="cover" />
+                <Image
+                  src="/images/slide03.jpg"
+                  w="100%"
+                  h="100%"
+                  objectFit="cover" 
+                  position="absolute"
+                />
+                 <Text
+                  fontSize="3xl"
+                  fontWeight="700"
+                  color="white"
+                  position="relative"
+                  align="center"
+                  mt="44"
+                >
+                  América do Sul<br/>
+                  O continente da tradição
+                </Text>
             </SwiperSlide>
         </Swiper>
     )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme }  from '../styles/theme'
 
-import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css"
 import "swiper/components/pagination/pagination.min.css"
 
